73c0c941aa63e0c71c7eaad21903d979cb4893e8
guozhao <davidkoojohn@163.com>
2021-06-10 23:15:38 +0800

    add vue-router@4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <div class="app">
+    <p>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link :to="{name: 'Task'}">Task</router-link>
+      |
+      <router-link to="/about">About</router-link>
+    </p>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,13 +19,19 @@ export default defineComponent({
 })
 </script>
 
+<style scoped>
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'Landing',
+    component: () => import('../views/Landing.vue')
+  },
+  {
+    path: '/',
+    name: 'Task',
+    component: () => import('../views/Task.vue')
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/About.vue')
+  }
+]
+
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+export default router
