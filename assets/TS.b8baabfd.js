1a2f8f1dbd63ba063b85ca02d86d0f0a468270f2
davidkoojohn <davidkoojohn@gmail.com>
2021-07-15 01:27:13 +0000

    Deploying to gh-pages from  @ 621841a30dcaf9569a8544c6cbb292ff7310934b 🚀

---
+++ assets/TS.b8baabfd.js
@@ -0,0 +1,1 @@
+import{d as a,o as e,c as r}from"./vendor.2a0da264.js";var s=a({setup(a){var s;return console.log((s={firstName:"123",lastName:"john"}).firstName+" - "+s.lastName),(a,s)=>(e(),r("h1",null,"TypeScript"))}});export default s;
