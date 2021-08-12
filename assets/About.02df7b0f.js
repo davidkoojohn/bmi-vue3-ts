d34a58970dc4d9a4a52766694bdb82484abe7d3a
davidkoojohn <davidkoojohn@gmail.com>
2021-08-12 09:54:50 +0000

    Deploying to gh-pages from  @ 4c770021b3af083f09b561cfbe31bdb1a18de2f1 🚀

---
+++ assets/About.02df7b0f.js
@@ -0,0 +1,1 @@
+import{o,p as r}from"./vendor.116f3db4.js";const n={};n.render=function(n,t){return o(),r("h1",null,"About")};export{n as default};
--- assets/About.5141589f.js
+++
@@ -1,1 +0,0 @@
-import{o,c as r}from"./vendor.2a0da264.js";const n={};n.render=function(n,t){return o(),r("h1",null,"About")};export default n;
